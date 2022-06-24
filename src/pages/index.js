b98ac5382156baf4484012a7fc8eae84ac5a3a0c
Nguyen Pham <ext-nguyen.pham@spdigital.sg>
2022-06-24 14:34:52 +0700

    init Project Todo UI

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Main from '../components/Home/Main'
+import Navbar from '../components/Home/Navbar'
+
+function Home() {
+  return (
+    <div className="flex w-full ">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  important: '#root',
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    // Remove Tailwind CSS's preflight style so it can use the MUI's preflight instead (CssBaseline).
+    preflight: false,
+  },
+  plugins: [],
+}
